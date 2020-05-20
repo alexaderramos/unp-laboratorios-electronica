d596bd19e18062e38dafe16ebde8a43579b6c145
Alexander Ramos <tcristian_15.go@hotmail.com>
2020-05-20 03:26:12 -0500

    breadcrumb component updated

--- resources/views/layouts/components/breadcrumb.blade.php
+++ resources/views/layouts/components/breadcrumb.blade.php
@@ -1,7 +1,12 @@
 <div class="col s10 m6 l6 breadcrumbs-left">
     <h5 class="breadcrumbs-title mt-0 mb-0 display-inline hide-on-small-and-down"><span>{{ $currentPage }}</span></h5>
     <ol class="breadcrumbs mb-0">
-        {{$links ?? ''}}
+        {{--$links ??  ''--}}
+        @if(($labels ?? '') !== '')
+            @foreach($labels as $label )
+                <li class="breadcrumb-item"><a href="{{route($label[1])}}">{{$label[0]}}</a></li>
+            @endforeach
+        @endif
         <li class="breadcrumb-item active">{{$currentPage}}</li>
     </ol>
 
